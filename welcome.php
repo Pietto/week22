e9bb8dd9a6cd9fb543299fbcee16425c604a07ee
Pieterjan van Dijk <Pieterjan.v.dijk@gmail.com>
2019-03-11 12:05:02 +0100

    start

---
+++ welcome.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>lab 1 - Welcome</title>
+    <link rel="stylesheet" type="text/css" href="welcome.css">
+</head>
+<body>
+    <div id="maindiv">
+        <div id="namediv">
+            <h1>Name</h1>
+            <input>
+        </div>
+
+        <div id="maildiv">
+            <h1>Email</h1>
+            <input>
+        </div>
+    </div>
+
+</body>
+</html>
